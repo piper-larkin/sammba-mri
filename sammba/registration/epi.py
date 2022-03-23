ac43835371b3473884d29d287984df6434bd7cca
Piper Larkin <97628499+piper-larkin@users.noreply.github.com>
2022-03-23 11:59:32 -0400

    Update epi.py
    Change sklearn.datasets.base to sklearn.datasets._base. Change .base to ._base

--- sammba/registration/epi.py
+++ sammba/registration/epi.py
@@ -1,7 +1,7 @@
 import warnings
-from sklearn.datasets.base import Bunch
+from sklearn.datasets._base import Bunch
 from nilearn._utils.exceptions import VisibleDeprecationWarning
-from .base import (_reorient, _rigid_body_register_and_reorient,
+from ._base import (_reorient, _rigid_body_register_and_reorient,
                    _per_slice_qwarp)
 
 
@@ -45,7 +45,7 @@ def _coregister_epi(unifized_anat_file,
 
     Returns
     -------
-    data : sklearn.datasets.base.Bunch
+    data : sklearn.datasets._base.Bunch
     Dictionary-like object, the interest attributes are :
         - `coreg_epi_` : str
                           Path to paths to the coregistered EPI image.
